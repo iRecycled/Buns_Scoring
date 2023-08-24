f5173cf7110a0184d5bbc4a62c8a459143fcebe6
Nick Peterson <npeterson1996@gmail.com>
2023-08-23 22:16:31 -0500

    edit of scoring

---
+++ resources/js/Scoring.js
@@ -0,0 +1,51 @@
+export default class Scoring {
+
+    constructor(){
+
+    }
+
+    init(){
+    function changeTab(){
+        console.log("hello");
+        }
+
+    document.getElementById('qualifying').addEventListener('click', function() {
+      console.log("clicked");
+    });
+
+    // Get all the list items
+    const listItems = document.querySelectorAll('.tab-link');
+
+    // Get the content container
+    const contentContainer = document.getElementById('content');
+
+    // Add click event listeners to each list item
+    listItems.forEach(item => {
+        item.addEventListener('click', () => {
+            // Get the value of the 'data-tab' attribute
+            const tab = item.getAttribute('data-tab');
+            console.log(tab);
+
+            // Update the content based on the clicked tab
+            if (tab === 'qualifying') {
+                contentContainer.textContent = 'Qualifying content here.';
+            } else if (tab === 'heats') {
+                contentContainer.textContent = 'Heats content here.';
+            } else if (tab === 'consolation') {
+                contentContainer.textContent = 'Consolation content here.';
+            } else if (tab === 'feature') {
+                contentContainer.textContent = 'Feature content here.';
+            }
+
+            // Remove the 'active' class from all items and add it to the clicked item
+            listItems.forEach(item => {
+                item.classList.remove('active');
+            });
+            item.classList.add('active');
+        });
+    });
+    }
+}
+    // Create an instance of the class and initialize it
+    const scoringInstance = new Scoring();
+    scoringInstance.init();
